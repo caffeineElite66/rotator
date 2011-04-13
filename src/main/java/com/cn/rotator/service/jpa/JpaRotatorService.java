0e91ca8a31e727e3755165f28aa0711d7fae7308
U-T410\kevin <kevin@T410.(none)>
2011-04-13 16:04:42 -0700

    working version of Rest

--- src/main/java/com/cn/rotator/service/jpa/JpaRotatorService.java
+++ src/main/java/com/cn/rotator/service/jpa/JpaRotatorService.java
@@ -2,13 +2,18 @@ package com.cn.rotator.service.jpa;
 
 import com.cn.rotator.domain.dao.RotatorDao;
 import com.cn.rotator.domain.dom.Rotator;
+import com.cn.rotator.domain.dom.RotatorDestinationPath;
 import com.cn.rotator.service.RotatorService;
 import java.net.MalformedURLException;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
+import java.util.Random;
+
 import javax.inject.Inject;
 import java.net.URL;
+import java.util.Iterator;
+import java.util.List;
 
 @Service("rotatorService")
 @Repository
@@ -23,8 +28,18 @@ public class JpaRotatorService implements RotatorService {
 
     @Override
     public URL getDestinationPath(long rid) throws MalformedURLException {
-        throw new UnsupportedOperationException("Not supported yet.");
+        Rotator r = rotatorDao.findRotatorById(rid);
+        List paths = r.getRotatorDestinationPaths();
+        Random random = new Random();
+        int number = random.nextInt(100) + 1;
+        int weight = 0;
+        for (Iterator i = paths.iterator(); i.hasNext();) {
+            RotatorDestinationPath path = (RotatorDestinationPath) i.next();
+            if (number > weight && number <= (path.getPathPercent() + weight)) {
+                return new URL(path.getUrl());
+            }
+            weight += path.getPathPercent();
+        }
+        throw new RuntimeException("No Path was found with weight: " + weight);
     }
-
-
 }
